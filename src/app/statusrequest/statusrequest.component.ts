f153a178a740debc8720650b37b2be667cb94b87
DAMMIKA940 <dammikapadmasiri123@gmail.com>
2022-05-04 22:42:14 +0530

    dashbpord ui add

---
+++ src/app/statusrequest/statusrequest.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-statusrequest',
+  templateUrl: './statusrequest.component.html',
+  styleUrls: ['./statusrequest.component.css']
+})
+export class StatusrequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
