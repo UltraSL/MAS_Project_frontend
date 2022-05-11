7e542412e1e418863be8be36122243ca5caf44f2
DAMMIKA940 <dammikapadmasiri123@gmail.com>
2022-05-11 10:37:36 +0530

    user update

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -2,76 +2,96 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { Subject } from 'rxjs';
+import { UserProfile } from '../app/models/user-profile';
 const httpOptions = {
-  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
 };
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  userData: any = {};
 
-  userData : any = {}
+  private _loginUrl = 'http://localhost:8000/user/Login';
+  private _forgetPasswordUrl = 'http://localhost:8000/user/forgotpassword';
+  private _putUrl = 'http://localhost:8000/user/EditUser/';
+  private _getImage = 'http://localhost:8000/user/profile/';
+  private profiles: UserProfile[] = [];
+  private profiles$ = new Subject<UserProfile[]>();
 
-  private _loginUrl ="http://localhost:8000/user/Login";
-  private _forgetPasswordUrl ="http://localhost:8000/user/forgotpassword";
-  private _putUrl ="http://localhost:8000/user/EditUser/";
-
-  constructor(private http : HttpClient 
-  ) { }
+  constructor(private http: HttpClient) {}
 
   loginUser(user: any) {
-    return this.http.post<any>(this._loginUrl, user)
+    return this.http.post<any>(this._loginUrl, user);
   }
 
   addUser(user: any) {
-    return this.http.post<any>("http://localhost:8000/user/AddUser", user)
+    return this.http.post<any>('http://localhost:8000/user/AddUser', user);
   }
-  sendEmail(email: any){
-    return this.http.post<any>(this._forgetPasswordUrl, email)
+  sendEmail(email: any) {
+    return this.http.post<any>(this._forgetPasswordUrl, email);
   }
 
-  empLoggedIn(){
-
+  empLoggedIn() {
     this.userData = JSON.parse(localStorage.getItem('user') || '{}');
-    if(this.userData.position=="employee"){
+    if (this.userData.position == 'employee') {
       return true;
     }
     return false;
   }
 
-  mngLoggedIn(){
-
+  mngLoggedIn() {
     this.userData = JSON.parse(localStorage.getItem('user') || '{}');
-    if(this.userData.position=="manager"){
+    if (this.userData.position == 'manager') {
       return true;
     }
     return false;
   }
 
-  TransportMngLoggedIn(){
-
+  TransportMngLoggedIn() {
     this.userData = JSON.parse(localStorage.getItem('user') || '{}');
-    if(this.userData.position=="transporter"){
+    if (this.userData.position == 'transporter') {
       return true;
     }
     return false;
   }
 
-
-  updateUserDetails(user : any){
+  updateUserDetails(user: any) {
     let body = new HttpParams()
-    .set('firstName', user.firstName)
-    .set('lastName', user.lastName)
-    .set('email', user.email)
-    .set('position', user.position)
-    .set('department', user.department)
-    .set('supervisorName', user.supervisorName)
-    .set('mobile', user.mobile);
-    console.log(user+"user in auth")
-    return this.http.put<any>(`${this._putUrl}${user._id}`, body.toString(),{
-      headers: new HttpHeaders()
-      .set('Content-Type', 'application/x-www-form-urlencoded')
+      .set('firstName', user.firstName)
+      .set('lastName', user.lastName)
+      .set('email', user.email)
+      .set('position', user.position)
+      .set('department', user.department)
+      .set('supervisorName', user.supervisorName)
+      .set('mobile', user.mobile);
+    console.log(user + 'user in auth');
+    return this.http.put<any>(`${this._putUrl}${user._id}`, body.toString(), {
+      headers: new HttpHeaders().set(
+        'Content-Type',
+        'application/x-www-form-urlencoded'
+      ),
     });
   }
 
+  uploadImage(id : string,image: File): void {
+    const profileData = new FormData();
+
+    profileData.append('image', image);
+    this.http
+      .put<{ profile: UserProfile }>('http://localhost:8000/user/EditUser/'+id ,profileData)
+      .subscribe((profileData) => {
+        const profile: UserProfile = {
+          image: profileData.profile.image,
+        };
+        this.profiles.push(profile);
+        this.profiles$.next(this.profiles);
+      });
+  }
+
+
+  getUserImageById(id :any){
+    return this.http.get<any>(`${this._getImage}${id}`)
+  }
 }
